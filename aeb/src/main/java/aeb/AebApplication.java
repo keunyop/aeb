6f149a6b847ecb6cb7e6cc7f04b8d44b842f3432
keunyop <kylee1112@hotmail.com>
2022-07-03 02:15:03 +0900

    Initial commit

---
+++ aeb/src/main/java/aeb/AebApplication.java
@@ -0,0 +1,13 @@
+package aeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AebApplication.class, args);
+	}
+
+}
---
+++ aeb/src/test/java/aeb/AebApplicationTests.java
@@ -0,0 +1,13 @@
+package aeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
